5ab4535f1cbfad94e34227b8ddc8919eec738ca9
Daniel Oliveira <daniel.anjos@protonmail.com>
2022-05-24 16:19:33 -0300

    V0.2

--- Hora-do-Dia/script.js
+++ Hora-do-Dia/script.js
@@ -5,16 +5,21 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     var minuto = data.getMinutes()
+    let dia = String(data.getDate()).padStart(2, '0');
+    let mes = String(data.getMonth() + 1).padStart(2, '0');
+    let ano = data.getFullYear()
+    dataAtual = dia + '/' + mes + '/' + ano;
     msg.innerHTML = `Agora sao ${hora} horas e ${minuto} minutos.`
+    msg2.innerHTML = `Data: ${dataAtual}`
     if (hora >= 0 && hora < 12) {
         //BOM DIA!
         img.src = 'dia.png'
         document.body.style.background = '#fab876'
-    }else if (hora >= 12 && hora <= 18) {
+    } else if (hora >= 12 && hora <= 18) {
         //BOA TARDE!
         img.src = 'tarde.png'
         document.body.style.background = '#6a476a'
-    }else{
+    } else {
         //BOA NOITE!
         img.src = 'noite.png'
         document.body.style.background = '#192329'
